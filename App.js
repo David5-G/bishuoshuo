19b1d8195e11fadcfdba64559e488b50c2aa88ca
Lili <l@Lili.local>
2019-01-17 09:41:37 +0800

    xcode 正常运行

--- App.js
+++ App.js
@@ -22,6 +22,8 @@ export default class App extends Component<Props> {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
+        <Text style={styles.welcome}>Welcome to React Native!</Text>
         <Text style={styles.instructions}>To get started, edit App.js</Text>
         <Text style={styles.instructions}>{instructions}</Text>
       </View>
