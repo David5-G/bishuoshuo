6837ce95070dc0e44bf09f5e3253f27c117f1d4f
Lili <l@Lili.local>
2019-02-08 09:09:24 +0800

    react native video 添加完成

--- src/views/teach/player.js
+++ src/views/teach/player.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { View, Text, Button, Dimensions, TouchableOpacity,WebView, ScrollView, Image, StyleSheet } from 'react-native';
+import { View, Text, Button, Dimensions, TouchableOpacity, WebView, ScrollView, Image, StyleSheet } from 'react-native';
 import NavigationBar from '../common/NavigationBar'
 import Icon from 'react-native-vector-icons/Ionicons'
 import Colors from '../../constants/Colors'
-
+import Video from 'react-native-video';
 export default class Player extends React.Component {
     static navigationOptions = {
         title: 'Player',
@@ -28,7 +28,25 @@ export default class Player extends React.Component {
                     style={{}}
                     leftButton={<Icon style={{ paddingLeft: 20, paddingRight: 20 }} onPress={() => navigation.goBack()} name={'ios-arrow-back'} size={28} color={Colors.headerText} />}
                 />
-                
+                {/* <Video source={{ uri: "Url" }}   // Can be a URL or a localfile.
+                    ref={(ref) => {
+                        this.player = ref
+                    }}                                      // Store reference
+                    onBuffer={this.onBuffer}                // Callback when remote video is buffering
+                    onEnd={this.onEnd}                      // Callback when playback finishes
+                    onError={this.videoError}               // Callback when video cannot be loaded
+                    style={styles.backgroundVideo} /> */}
+
+                <Video
+                    source={{ uri: 'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4' }}
+                    rate={1.0}
+                    volume={1.0}
+                    isMuted={false}
+                    resizeMode="cover"
+                    shouldPlay
+                    isLooping
+                    style={{ width: 300, height: 300 }}
+                />
             </View>
         );
     }
@@ -39,11 +57,11 @@ const styles = StyleSheet.create({
         flex: 1,
         backgroundColor: '#fff',
     },
-    pic: {
-        marginTop: 20,
-        borderRadius: 4,
-        shadowColor: '#000',
-		shadowOpacity: 0.15,
-		shadowOffset: { width: 0, height: 2 },
-    }
+    backgroundVideo: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        bottom: 0,
+        right: 0,
+    },
 });
